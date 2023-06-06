1bc7da07b8b0f5e4b0489aa9d09c0b893354e093
Kaleigh Collins <klee.rene2@gmail.com>
2023-06-06 07:49:45 -0700

    first commit

---
+++ II/practice.py
@@ -0,0 +1,15 @@
+# def lastnames(first):
+# 	#for every first name entered, return last name
+# 	return {
+# 		'Kaleigh': 'Collins',
+# 		'Travis': 'Tompkins',
+# 		'Clayton': 'Tompkins',
+# 		'Kenesha': 'Brooks',
+# 		'Dylan': 'Bell',
+# 		}[first]
+# print(lastnames('Travis'))
+
+def area(length, width):
+    length = 10
+    width = 12
+    print(length * width)
